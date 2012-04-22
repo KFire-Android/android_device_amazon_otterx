f8415b4e7f3749f98b107653021c17e558877e72
Hashcode <hashcode0f@gmail.com>
2012-04-22 15:09:06 -0700

    bugfix new MAC script

--- prebuilt/bin/fix-mac.sh
+++ prebuilt/bin/fix-mac.sh
@@ -6,7 +6,7 @@ NVS_BIN=/system/etc/firmware/ti-connectivity/wl1271-nvs.bin
 if [ ! -f "$NVS_BIN" ]; then
     busybox mount -o remount,rw /system
     cp ${NVS_BIN}.orig ${NVS_BIN}
-    calibrator set nvs_mac $NVS_BIN $(dmesg|grep -i die-id|cut -c36-|sed -e 's/)//g'|sed -e 's/-//g'|sed 's/../&:/g;s/:$//')
+    calibrator set nvs_mac $NVS_BIN F0:A2:25:$(dmesg|grep -i die-id|cut -c43-|sed -e 's/)//g'|sed -e 's/-//g'|sed 's/../&:/g;s/:$//')
     busybox mount -o remount,ro /system
 fi
 
