bc57df876edc91fb0d9006abbbaea4c0a63322be
Andrew Dodd <atd7@cornell.edu>
2012-05-06 17:19:09 -0400

    Enable Google network location provider
    Change-Id: If493a83011376e3a21d14248b03485d60614f26a

--- device.mk
+++ device.mk
@@ -192,6 +192,12 @@ PRODUCT_CHARACTERISTICS := tablet,nosdcard
 
 DEVICE_PACKAGE_OVERLAYS := device/amazon/otter/overlay
 
+# enable Google-specific location features,
+# like NetworkLocationProvider and LocationCollector
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.com.google.locationfeatures=1 \
+    ro.com.google.networklocation=1
+
 # we have enough storage space to hold precise GC data
 PRODUCT_TAGS += dalvik.gc.type-precise
 
