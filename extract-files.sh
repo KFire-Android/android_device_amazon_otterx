e7c4f42fd141f09075a38367e99c07ccab0f1a41
Hashcode <hashcode0f@gmail.com>
2013-02-06 22:04:55 -0800

    setup device/vendor extract files
    Change-Id: I60450665b8c28c819c8c97c3d25c8626e24767b8

---
+++ extract-files.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+
+VENDOR=amazon
+DEVICE=otter
+
+BASE=../../../vendor/$VENDOR/$DEVICE/proprietary
+
+echo "Pulling $DEVICE files..."
+for FILE in `cat proprietary-files.txt | grep -v ^# | grep -v ^$`; do
+DIR=`dirname $FILE`
+    if [ ! -d $BASE/$DIR ]; then
+mkdir -p $BASE/$DIR
+    fi
+adb pull /system/$FILE $BASE/$FILE
+done
+
+./setup-makefiles.sh
+
+# Call up to otter-common
+../otter-common/extract-files.sh
