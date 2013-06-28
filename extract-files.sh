c7e071ec7f2913304ba2ce771198df703c2985be
Hashcode <hashcode0f@gmail.com>
2013-06-27 22:06:51 -0700

    simplify vendor props
    Change-Id: Id272156109f3ac90ed28966b9112a3fa5099d4c1

--- extract-files.sh
+++ extract-files.sh
@@ -1,21 +1,5 @@
 #!/bin/sh
 
-VENDOR=amazon
-DEVICE=otter
-
-BASE=../../../vendor/$VENDOR/$DEVICE/proprietary
-
-echo "Pulling $DEVICE files..."
-for FILE in `cat proprietary-files.txt | grep -v ^# | grep -v ^$`; do
-DIR=`dirname $FILE`
-    if [ ! -d $BASE/$DIR ]; then
-mkdir -p $BASE/$DIR
-    fi
-adb pull /system/$FILE $BASE/$FILE
-done
-
-./setup-makefiles.sh
-
-# Call up to otter-common
-cd ../otter-common
+# Call up to omap4-common
+cd ../omap4-common
 ./extract-files.sh
