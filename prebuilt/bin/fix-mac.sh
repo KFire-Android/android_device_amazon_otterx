b2718290d1249b370685fcddc045c49ccf5a6ce2
Hashcode <hashcode0f@gmail.com>
2014-04-30 13:30:49 -0700

    fix-mac: remove prebuilt references

--- prebuilt/bin/fix-mac.sh
+++ prebuilt/bin/fix-mac.sh
@@ -4,10 +4,10 @@ PATH=/sbin:/vendor/bin:/system/sbin:/system/bin:/system/xbin
 ORIG_NVS_BIN=/system/etc/firmware/ti-connectivity/wl1271-nvs_127x.bin
 NVS_BIN=/system/etc/firmware/ti-connectivity/wl1271-nvs.bin
 
-if bbx [ ! -f "$NVS_BIN" ]; then
-    bbx mount -o remount,rw /system
-    bbx cp ${ORIG_NVS_BIN} ${NVS_BIN}
-    calibrator set nvs_mac $NVS_BIN $(idme ?| bbx grep 0x0030 | bbx cut -c35-46| bbx sed -e 's/)//g'| bbx sed -e 's/-//g'| bbx sed 's/../&:/g;s/:$//')
-    bbx mount -o remount,ro /system
+if [ ! -f "$NVS_BIN" ]; then
+    mount -o remount,rw /system
+    cp ${ORIG_NVS_BIN} ${NVS_BIN}
+    calibrator set nvs_mac $NVS_BIN $(getprop ro.boot.wifimac | sed 's/../&:/g;s/:$//')
+    mount -o remount,ro /system
 fi
 
